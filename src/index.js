e8c8f2fd20e0a7116ef0a6e9982d4fe38a16f941
Duy Hung <90083163+DuyHung03@users.noreply.github.com>
2023-04-28 09:46:56 +0700

    s

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-    BrowserRouter,
-    HashRouter,
-} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import GlobalStyles from './components/GlobalStyles';
 
