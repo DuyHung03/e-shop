82650675e16539f8a9077874d1e1baaa0cc40297
Duy Hung <90083163+DuyHung03@users.noreply.github.com>
2023-04-28 12:00:07 +0700

    publicurl

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import GlobalStyles from './components/GlobalStyles';
+
+const routerBaseName = process.env.PUBLIC_URL;
 
 const root = ReactDOM.createRoot(
     document.getElementById('root'),
 );
 root.render(
-    <HashRouter>
+    <BrowserRouter basename={routerBaseName}>
         <GlobalStyles>
             <App />
         </GlobalStyles>
-    </HashRouter>,
+    </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
